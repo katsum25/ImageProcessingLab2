e3d01ba428ad75ccbbc53271e435b6352787aea5
Evgenii Napylov <61109747+Talesard@users.noreply.github.com>
2020-10-21 21:45:39 +0300

    mu=20

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     img = cv2.imread('images/oka.jpg')
     cv2.imshow('Image', img)
     cv2.waitKey(0)
-    cv2.imshow('Image', add_gaussian_noise(image=img, mu=2, sigma=15))
+    cv2.imshow('Image', add_gaussian_noise(image=img, mu=20, sigma=15))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
